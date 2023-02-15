6bb3cc0771d2753ba90d2d5b45cc33e294b54a82
hedevelope <batujano@gmail.com>
2023-02-15 14:25:33 +0300

    changes

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,15 +8,6 @@ import { loaderDelay } from '@utils';
 import { useScrollDirection, usePrefersReducedMotion } from '@hooks';
 import { Menu } from '@components';
 import { IconLogo } from '@components/icons';
-import mixpanel from 'mixpanel-browser';
-
-mixpanel.init("948003552b5b41e2eed62cabd9916d5e" , {
-  debug: true
-})
-
-let butonClick =(e) => {
-  mixpanel.track("Resume clicked.")
-}
 
 const StyledHeader = styled.header`
   ${({ theme }) => theme.mixins.flexBetween};
